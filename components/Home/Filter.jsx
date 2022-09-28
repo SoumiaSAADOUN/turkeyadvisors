8180b21cba1ecd0d8115b2ec050a71d7cc385956
SoumiaSAADOUN <es_saadoun@esi.dz>
2022-09-28 22:30:28 +0100

    small fixe

--- components/Home/Filter.jsx
+++ components/Home/Filter.jsx
@@ -43,8 +43,8 @@ const Filter = () => {
         <h1 className="text-2xl font-semibold md:basis-2/6 sm:basis-full ">
           {t.home.dreamHome}
         </h1>
-        <div className=" flex flex-wrap justify-center w-full md:basis-4/6 sm:basis-full md:justify-end">
-          <div className=" relative flex flex-wrap lg:basis-2/3">
+        <div className="flex flex-wrap justify-center w-full md:basis-4/6 sm:basis-full md:justify-end">
+          <div className="relative flex flex-wrap lg:basis-2/3">
             <button
               onClick={() => setShowCityFilter(!showCityFilter)}
               className=" flex items-center justify-between lg:basis-1/4 basis-full grow mr-2 md:basis-1/4 mb-2  px-1 h-[40px] rounded-lg shadow-md "
@@ -64,25 +64,25 @@ const Filter = () => {
               </span>
             </button>
 
-            <div className=" lg:basis-1/4 basis-full grow mr-2 md:basis-1/4 mb-2  px-1 h-[40px] ">
+            <div onClick={()=> setShowCityFilter(false)}   className=" lg:basis-1/4 basis-full grow mr-2 md:basis-1/4 mb-2  px-1 h-[40px] ">
               <SelectComponenet data={propertyType} />
             </div>
-            <div className=" lg:basis-1/4 basis-full grow mr-2 md:basis-1/4 mb-2  px-1 h-[40px]   ">
-              <SelectComponenet data={bedrooms} />
+            <div onClick={()=> setShowCityFilter(false)} className=" lg:basis-1/4 basis-full grow mr-2 md:basis-1/4 mb-2  px-1 h-[40px]   ">
+              <SelectComponenet data={bedrooms}  />
             </div>
-            <button className="xl:hidden basis-full md:basis-1/4 grow mr-2  mb-2 flex px-4 py-2 h-[40px] justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR">
+            <button onClick={()=> setShowCityFilter(false)} className="xl:hidden basis-full md:basis-1/4 grow mr-2  mb-2 flex px-4 py-2 h-[40px] justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR">
               <HiOutlineSearch className="w-4 h-4 mr-2 text-xl font-bold" />
               {t.home.search}
             </button>
 
             <CityFilter
               visibility={showCityFilter}
-              onCancel={() => setShowCityFilter(false)}
+              onCancel={() => setShowCityFilter(!showCityFilter)}
              
             />
           </div>
 
-          <button className="mr-2 mb-2 xl:flex px-4 py-2 h-[40px] xl:basis-1/4 hidden  justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR">
+          <button onClick={()=> setShowCityFilter(false)} className="mr-2 mb-2 xl:flex px-4 py-2 h-[40px] xl:basis-1/4 hidden  justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR">
             <HiOutlineSearch className="w-4 h-4 mr-2 text-xl font-bold" />
             {t.home.search}
           </button>
