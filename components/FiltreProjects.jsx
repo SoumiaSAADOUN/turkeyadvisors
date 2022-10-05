33e289defab39a785555ef375a634007dc4c7726
SoumiaSAADOUN <es_saadoun@esi.dz>
2022-10-05 18:03:14 +0100

    projects 0.1

--- components/FiltreProjects.jsx
+++ components/FiltreProjects.jsx
@@ -1,41 +1,129 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+import SelectComponenet from "./Home/SelectComponenet";
+import CityFilter from "./Home/CityFilter";
+import { FaMapMarkerAlt, FaSortDown } from "react-icons/fa";
 
 const FiltreProjects = () => {
-    return <>
-        <div className="flex px-20 my-12 gap-4 flex-wrap">
-            <h1 className="text-PRIMARY_COLOR text-2xl font-bold">
-                Find Your Dream House
-            </h1>
-            <select
-                className="basis-1/8 grow  rounded-lg shadow-md caret-yellow-100"
-                defaultValue='City'
-            >
-                <option value='city'>City</option>
-            </select>
+  const [showCityFilter, setShowCityFilter] = useState(false);
+  const [showAreaFilter, setShowAreaFilter] = useState(false);
+  const cities = ["Istanbul", "Bodrum", "Çeşme", "Alaçatı"];
+  const areas = ["Bagcilar", "Bahcesehir", "Bakirkoy", "Basaksehir","Bayrampasa",'Besiktas',"Beykoz","Beylikduzu","Cekmekoy","Eyup"];
 
-            <select
-                className="basis-1/8 grow rounded-lg shadow-md caret-yellow-100"
-                defaultValue='Area'
-            >
-                <option value='city'>Area</option>
-            </select>
-            <select
-                className="basis-1/8 grow  rounded-lg shadow-md caret-yellow-100"
-                defaultValue='Property type'
-            >
-                <option value='city'>Property type</option>
-            </select>
-            <select
-                className="basis-1/8 grow rounded-lg shadow-md caret-yellow-100"
-                defaultValue='Bedrooms'
-            >
-                <option value='city'>Bedrooms</option>
-            </select>
-            <button className="flex basis-1/8 grow px-8 py-2 justify-between items-center font-bold text-xl rounded text-BACKGROUND_COLOR bg-PRIMARY_COLOR hover:text-PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-BACKGROUND_COLOR">
-                Search <HiOutlineSearch className="w-5 h-5  text-xl font-bold" />
+  const bedrooms = [
+    { title: "Bedrooms" },
+    { title: "1+0" },
+    { title: "1+1" },
+    { title: "2+1" },
+    { title: "3+1" },
+    { title: "4+1" },
+    { title: "More" },
+  ];
 
+  const propertyType = [
+    { title: "Property Type" },
+    { title: "Apartment" },
+    { title: "Villa" },
+    { title: "Land" },
+    { title: "Shop" },
+    { title: "Home Office" },
+    { title: "Offices" },
+  ];
+
+  return (
+    <>
+      <div className="flex justify-between w-full px-20 my-12">
+        <h1 className="text-2xl font-bold text-PRIMARY_COLOR">
+          Find Your Dream House
+        </h1>
+        <div className="z-10 flex justify-center w-full grow md:basis-4/6 sm:basis-full md:justify-end">
+          <div className="relative flex flex-wrap lg:basis-2/3">
+            <button
+              onClick={() => {setShowCityFilter(!showCityFilter); setShowAreaFilter(false)}}
+              className=" flex items-center justify-between lg:basis-1/5 basis-full grow mr-2 md:basis-1/5 mb-2  px-1 h-[40px] rounded-lg shadow-md "
+            >
+              <span className="flex items-center justify-center ">
+                <FaMapMarkerAlt
+                  className="w-4 h-4 mr-2 text-SECONDARY_COLOR"
+                  aria-hidden="true"
+                />
+                City
+              </span>
+              <span className="flex pb-2 ml-3 pointer-events-none ">
+                <FaSortDown
+                  className="w-5 h-5 text-SECONDARY_COLOR"
+                  aria-hidden="true"
+                />
+              </span>
             </button>
-        </div></>
-}
+
+            <button
+              onClick={() => {setShowAreaFilter(!showAreaFilter); setShowCityFilter(false)}}
+              className=" flex items-center justify-between lg:basis-1/5 basis-full grow mr-2 md:basis-1/5 mb-2  px-1 h-[40px] rounded-lg shadow-md "
+            >
+              <span className="flex items-center justify-center ">
+                <FaMapMarkerAlt
+                  className="w-4 h-4 mr-2 text-SECONDARY_COLOR"
+                  aria-hidden="true"
+                />
+                Area
+              </span>
+              <span className="flex pb-2 ml-3 pointer-events-none ">
+                <FaSortDown
+                  className="w-5 h-5 text-SECONDARY_COLOR"
+                  aria-hidden="true"
+                />
+              </span>
+            </button>
+
+            <div
+              onClick={() => {
+                setShowCityFilter(false);
+                setShowAreaFilter(false);
+              }}
+              className=" lg:basis-1/5 basis-1/3 grow lg:mr-2 md:basis-1/5 mb-2  px-1 h-[40px] "
+            >
+              <SelectComponenet data={propertyType} />
+            </div>
+            <div
+              onClick={() => {
+                setShowCityFilter(false);
+                setShowAreaFilter(false);
+              }}
+              className=" lg:basis-1/5 basis-1/3 grow mr-2 md:basis-1/5 mb-2  px-1 h-[40px]   "
+            >
+              <SelectComponenet data={bedrooms} />
+            </div>
+            <button
+              onClick={() => setShowCityFilter(false)}
+              className="xl:hidden basis-full md:basis-1/5 grow mr-2  mb-2 flex px-4 py-2 h-[40px] justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR"
+            >
+              <HiOutlineSearch className="w-4 h-4 mr-2 text-xl font-bold" />
+              Search
+            </button>
+
+            <CityFilter
+              data={cities}
+              visibility={showCityFilter}
+              onCancel={() => setShowCityFilter(!showCityFilter)}
+            />
+            <CityFilter
+              data={areas}
+              visibility={showAreaFilter}
+              onCancel={() => setShowAreaFilter(!showCityFilter)}
+            />
+          </div>
+
+          <button
+            onClick={() => setShowCityFilter(false)}
+            className="mr-2 mb-2 xl:flex px-4 py-2 h-[40px] xl:basis-1/5 hidden  justify-center items-center font-bold rounded-md text-BACKGROUND_COLOR bg-DARK_PRIMARY_COLOR hover:border-PRIMARY_COLOR hover:border hover:bg-PRIMARY_COLOR"
+          >
+            <HiOutlineSearch className="w-4 h-4 mr-2 text-xl font-bold" />
+            Search
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default FiltreProjects;
--- components/Home/CityFilter.jsx
+++ components/Home/CityFilter.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 const CityFilter = (props) => {
   const filters = ["Istanbul", "Bodrum", "Çeşme", "Alaçatı"];
-  
+console.log(props.title)
   return (
     <>
       {props.visibility ? (
-        <div  className="absolute left-0 z-10 flex flex-col w-full px-8 py-4 rounded-lg shadow-md top-11 bg-BACKGROUND_COLOR">
+        <div className="absolute left-0 z-10 flex flex-col w-full px-8 py-4 rounded-lg shadow-md top-11 bg-BACKGROUND_COLOR">
           <h1 className="w-fit">
             <span className="font-bold">
-              City
+              {props.title}
               <div className="flex-grow border-t-2 border-SECONDARY_COLOR"></div>
             </span>
           </h1>
-          <div className="flex flex-col lg:grid lg:grid-cols-2 lg:grid-rows-2">
-            {filters.map((item, index) => (
+          <div
+            className={`flex flex-col lg:grid ${
+              props.data.length >6? "lg:grid-cols-4" : "lg:grid-cols-2"
+            } `}
+          >
+            {props.data.map((item, index) => (
               <div className="flex my-4" key={`${item}-${index}`}>
                 <div className="flex items-center mr-3">
                   <input
